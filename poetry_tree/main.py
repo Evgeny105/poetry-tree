2c737691df2dc98e0f144ba46cf2452d2bf18fb7
Evgeny <gfmnlk@gmail.com>
2023-09-28 22:46:38 +0800

    pre-commit

--- poetry_tree/main.py
+++ poetry_tree/main.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score, mean_squared_error
 import pandas as pd
 from tree import *
+
 import pickle
 
 """#### Simple check"""
