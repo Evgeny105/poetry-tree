b864361099b93e819d748926cf0128a71873cf4c
Evgeny <gfmnlk@gmail.com>
2023-10-02 17:36:23 +0800

    bug fix

--- poetry_tree/infer.py
+++ poetry_tree/infer.py
@@ -6,26 +6,26 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 
-RANDOM_STATE = 42
+if __name__ == "__main__":
+    RANDOM_STATE = 42
 
-digits_data = load_digits().data
-digits_target = load_digits().target[:, None]
-X_train, X_test, y_train, y_test = train_test_split(
-    digits_data, digits_target, test_size=0.2, random_state=RANDOM_STATE
-)
+    digits_data = load_digits().data
+    digits_target = load_digits().target[:, None]
+    X_train, X_test, y_train, y_test = train_test_split(
+        digits_data, digits_target, test_size=0.2, random_state=RANDOM_STATE
+    )
 
-with open("data.pickle", "rb") as f:
-    class_estimator = pickle.load(f)
+    with open("data.pickle", "rb") as f:
+        class_estimator = pickle.load(f)
 
-answer = class_estimator.predict(X_test)
-accuracy = accuracy_score(y_test, answer)
-print("Accuracy of tree is", accuracy)
+    answer = class_estimator.predict(X_test)
+    accuracy = accuracy_score(y_test, answer)
+    print("Accuracy of tree is", accuracy)
 
+    with open("results.csv", "w", newline="") as csvfile:
+        fieldnames = ["reference", "answer"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-with open("results.csv", "w", newline="") as csvfile:
-    fieldnames = ["reference", "answer"]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-    writer.writeheader()
-    for ref, ans in zip(y_test, answer):
-        writer.writerow({"reference": ref[0], "answer": ans})
+        writer.writeheader()
+        for ref, ans in zip(y_test, answer):
+            writer.writerow({"reference": ref[0], "answer": ans})
--- poetry_tree/train.py
+++ poetry_tree/train.py
@@ -6,16 +6,17 @@ from sklearn.model_selection import train_test_split
 from tree import DecisionTree
 
 
-RANDOM_STATE = 42
+if __name__ == "__main__":
+    RANDOM_STATE = 42
 
-digits_data = load_digits().data
-digits_target = load_digits().target[:, None]
-X_train, X_test, y_train, y_test = train_test_split(
-    digits_data, digits_target, test_size=0.2, random_state=RANDOM_STATE
-)
+    digits_data = load_digits().data
+    digits_target = load_digits().target[:, None]
+    X_train, X_test, y_train, y_test = train_test_split(
+        digits_data, digits_target, test_size=0.2, random_state=RANDOM_STATE
+    )
 
-class_estimator = DecisionTree(max_depth=10, criterion_name="gini")
-class_estimator.fit(X_train, y_train)
+    class_estimator = DecisionTree(max_depth=10, criterion_name="gini")
+    class_estimator.fit(X_train, y_train)
 
-with open("data.pickle", "wb") as f:
-    pickle.dump(class_estimator, f)
+    with open("data.pickle", "wb") as f:
+        pickle.dump(class_estimator, f)
