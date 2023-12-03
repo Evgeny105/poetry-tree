38e790686111f5e84bc003c0e31e837180ca3144
Evgeny <gfmnlk@gmail.com>
2023-12-04 02:11:00 +0800

    run_server

--- commands.py
+++ commands.py
@@ -2,6 +2,7 @@ import fire
 from hydra import compose, initialize
 
 from poetry_tree.infer import main as infer_py
+from poetry_tree.run_onnx import main as run_onnx_py
 from poetry_tree.train import main as train_py
 
 
@@ -17,11 +18,18 @@ def infer():
     infer_py(cfg)
 
 
+def run_onnx():
+    with initialize(version_base="1.3.2", config_path="./config"):
+        cfg = compose(config_name="config")
+    run_onnx_py(cfg)
+
+
 def main():
     fire.Fire(
         {
             "infer": infer,
             "train": train,
+            "run_server": run_onnx,
         }
     )
 
